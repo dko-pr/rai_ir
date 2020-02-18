7a6812e8c12e6ab81285a46b0e687ceb573ea7e0
Denis Kotov <dkotov@transinform.info>
2020-02-18 12:46:45 +0300

    correct ip address to '10.10.200.24'

--- rai_ir.py
+++ rai_ir.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 api = Api(app)
 api.add_resource(rai_ir_resource.Tracking, "/rai.ir", "/rai.ir/", "/rai.ir/<int:carNumber>")
 if __name__ == '__main__':
-    app.run(host= ' 10.10.200.24', debug=True, port="21523")
+    app.run(host= '10.10.200.24', debug=True, port="21523")
