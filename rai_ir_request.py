34926d047536eb87df9ed4aaa1749a6c95191b92
Denis Kotov <dkotov@transinform.info>
2020-03-02 09:56:42 +0300

    clean code

--- rai_ir_request.py
+++ rai_ir_request.py
@@ -5,57 +5,39 @@ import jdatetime
 def getTrackingDetails(carNumber):
     login = "Guest"
     password = "123"
-
-    date = str(jdatetime.date.today())[2:].replace('-', '')
+    current_date = str(jdatetime.date.today())[2:].replace('-', '')
     url_check = r"http://customers.rai.ir/Main/check.asp"
-    url_detailed_info = r"http://customers.rai.ir/Wagon_Info/Tracking/Details.asp?WagonNO=" + str(carNumber) + "&Date=" + str(date)
+    url_detailed_info = r"http://customers.rai.ir/Wagon_Info/Tracking/Details.asp?WagonNO=" + str(carNumber) + "&Date=" + str(current_date)
 
     str_class_bs4_tag = "<class 'bs4.element.Tag'>"
+    html_fields = ['Id', 'CarNumber', 'TrainNumber', 'StationName', 'ArriveDate', 'ArriveTime', 'DepartDate', 'DepartTime', 'FromStation', 'DestStation']
 
     session = requests.Session()
-    session.post(url_check, data={'txtUserName': 'Guest', 'txtPassword': '123'})
+    session.post(url_check, data={'txtUserName': login, 'txtPassword': password})
     response = session.get(url_detailed_info)
     response.encoding = 'windows-1256'
 
-    big_soup = bs4(response.text, 'lxml')
-    fathers  = big_soup.find_all(id='AutoNumber2')
+    json_answer_empty = [{}]
+    full_answer = bs4(response.text, 'lxml')
+    fathers  = full_answer.find_all(id='AutoNumber2')
     if (fathers):
         father = fathers[0]
-        list = []
-        i = 0
+        json_answer_list = []
+        childs_counter = 0
         for first_child in father.children:
-            if i > 3:
+            if childs_counter > 3:
                 if str(type(first_child)) == str_class_bs4_tag:
-                    dict = {}
-                    j=0
+                    json_answer_dict = {}
+                    field_order_position = 0
                     for second_child in first_child.children:
                         if str(type(second_child)) == str_class_bs4_tag:
-                            small_soup = bs4(str(second_child), 'lxml')
-                            font_tag = small_soup.find('font')
-                            value = font_tag.text.strip()
-                            if (j == 0):
-                                dict['Id'] = value
-                            elif (j == 1):
-                                dict['CarNumber'] = value
-                            elif (j == 2):
-                                dict['TrainNumber'] = value
-                            elif (j == 3):
-                                dict['StationName'] = value
-                            elif (j == 4):
-                                dict['ArriveDate'] = value
-                            elif (j == 5):
-                                dict['ArriveTime'] = value
-                            elif (j == 6):
-                                dict['DepartDate'] = value
-                            elif (j == 7):
-                                dict['DepartTime'] = value
-                            elif (j == 8):
-                                dict['FromStation'] = value
-                            elif (j == 9):
-                                dict['DestStation'] = value
-                            j+=1
-                    list.append(dict)
-            i+=1
-        return str(list)
+                            one_operation = bs4(str(second_child), 'lxml')
+                            wrapper = one_operation.find('font')
+                            value = wrapper.text.strip()
+                            json_answer_dict[html_fields[field_order_position]] = value
+                            field_order_position += 1
+                    json_answer_list.append(json_answer_dict)
+            childs_counter += 1
+        return str(json_answer_list)
     else:
-        return str([{}])
+        return str(json_answer_empty)
