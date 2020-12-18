7a98120e9e2b5088c91994a2ab44af89262a8056
Denis Kotov <dkotov@transinform.info>
2020-12-18 18:20:44 +0300

    Актулаизирована работа с API, в т.ч. добавлено распознавание каптчи с помощью обученной нейронной сети

--- rai_ir_request.py
+++ rai_ir_request.py
@@ -1,24 +1,34 @@
 from bs4 import BeautifulSoup as bs4
 import requests
 import jdatetime
+import solve_captchas_with_model
 
 def getTrackingDetails(carNumber):
     login = "Guest"
     password = "123"
     current_date = str(jdatetime.date.today())[2:].replace('-', '')
-    url_check = r"http://customers.rai.ir/Main/check.asp"
-    url_detailed_info = r"http://customers.rai.ir/Wagon_Info/Tracking/Details.asp?WagonNO=" + str(carNumber) + "&Date=" + str(current_date)
+    url_main = r"http://customers.rai.ir/Main/Main.asp"
+    url_captcha = r"https://customers.rai.ir/Main/captcha.asp"
+    url_check = r"https://customers.rai.ir/Main/check.asp"
+    url_detailed_info = r"https://customers.rai.ir/Wagon_Info/Tracking/Details.asp?WagonNO=" + str(carNumber) + "&Date=" + str(current_date)
+    image_captcha_file = 'captcha.bmp'
 
     str_class_bs4_tag = "<class 'bs4.element.Tag'>"
     html_fields = ['Id', 'CarNumber', 'TrainNumber', 'StationName', 'ArriveDate', 'ArriveTime', 'DepartDate', 'DepartTime', 'FromStation', 'DestStation']
 
     session = requests.Session()
-    session.post(url_check, data={'txtUserName': login, 'txtPassword': password})
-    response = session.get(url_detailed_info)
-    response.encoding = 'windows-1256'
+    img_captcha_data = session.get(url_captcha).content
+    with open(image_captcha_file, 'wb') as handler:
+        handler.write(img_captcha_data)
+
+    captcha_text = solve_captchas_with_model.solve_captcha_from_file(image_captcha_file)
+    response_check = session.post(url_check, data={'txtUserName': login, 'txtPassword': password, 'txtCaptcha': captcha_text, 'B1':'%E6%D1%E6%CF'})
+
+    response_detailed_info = session.get(url_detailed_info)
+    response_detailed_info.encoding = 'windows-1256'
 
     json_answer_empty = [{}]
-    full_answer = bs4(response.text, 'lxml')
+    full_answer = bs4(response_detailed_info.text, 'lxml')
     fathers  = full_answer.find_all(id='AutoNumber2')
     if (fathers):
         father = fathers[0]
@@ -41,3 +51,5 @@ def getTrackingDetails(carNumber):
         return str(json_answer_list)
     else:
         return str(json_answer_empty)
+
+print(getTrackingDetails(28857845))
