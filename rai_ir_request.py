e7ba46f2adca7b3e73472ee234938115a6df3a03
Denis Kotov <dkotov@transinform.info>
2021-09-24 18:09:33 +0300

    Disable debug mode. Remove unnecessary lines.

--- rai_ir_request.py
+++ rai_ir_request.py
@@ -2,35 +2,29 @@ from bs4 import BeautifulSoup as bs4
 import requests
 import jdatetime
 import solve_captchas_with_model
-import time
 
 def getTrackingDetails(carNumber):
     json_answer_empty = [{}]
     login = "Guest"
     password = "123"
-    print(jdatetime.date.today())
     current_date = str(jdatetime.date.today())[2:].replace('-', '')
     # url_main = r"http://customers.rai.ir/Main/Main.asp"
     url_captcha = r"https://customers.rai.ir/Main/captcha.asp"
     url_check = r"https://customers.rai.ir/Main/check.asp"
     url_detailed_info = r"https://customers.rai.ir/Wagon_Info/Tracking/Details.asp?WagonNO=" + str(carNumber) + "&Date=" + str(current_date)
     image_captcha_file = 'captcha.bmp'
-
     str_class_bs4_tag = "<class 'bs4.element.Tag'>"
     html_fields = ['Id', 'CarNumber', 'TrainNumber', 'StationName', 'ArriveDate', 'ArriveTime', 'DepartDate', 'DepartTime', 'FromStation', 'DestStation']
-
     session = requests.Session()
-    response_captcha = session.get(url_captcha, verify=False)
+    response_captcha = session.get(url_captcha, verify=False, timeout=(5, 30))
     if response_captcha.status_code == 200:
         img_captcha_data = response_captcha.content
         with open(image_captcha_file, 'wb') as handler:
             handler.write(img_captcha_data)
-
         captcha_text = solve_captchas_with_model.solve_captcha_from_file(image_captcha_file)
         response_check = session.post(url_check, data={'txtUserName': login, 'txtPassword': password, 'txtCaptcha': captcha_text, 'B1':'%E6%D1%E6%CF'}, timeout=(5, 30))
-
         if response_check.status_code == 200:
-            response_detailed_info = session.get(url_detailed_info)
+            response_detailed_info = session.get(url_detailed_info, timeout=(5, 30))
             response_detailed_info.encoding = 'windows-1256'
             if response_detailed_info.status_code == 200:
                 full_answer = bs4(response_detailed_info.text, 'lxml')
