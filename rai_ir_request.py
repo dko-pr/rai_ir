be7ee379e43481c141e37701ac7a4f22c6b67400
Denis Kotov <dkotov@transinform.info>
2021-09-24 17:29:23 +0300

    Add additional check. Correct an error. And add some records to .gitignore

--- rai_ir_request.py
+++ rai_ir_request.py
@@ -5,6 +5,7 @@ import solve_captchas_with_model
 import time
 
 def getTrackingDetails(carNumber):
+    json_answer_empty = [{}]
     login = "Guest"
     password = "123"
     print(jdatetime.date.today())
@@ -19,39 +20,42 @@ def getTrackingDetails(carNumber):
     html_fields = ['Id', 'CarNumber', 'TrainNumber', 'StationName', 'ArriveDate', 'ArriveTime', 'DepartDate', 'DepartTime', 'FromStation', 'DestStation']
 
     session = requests.Session()
-    img_captcha_data = session.get(url_captcha, verify=False).content
-    with open(image_captcha_file, 'wb') as handler:
-        handler.write(img_captcha_data)
+    response_captcha = session.get(url_captcha, verify=False)
+    if response_captcha.status_code == 200:
+        img_captcha_data = response_captcha.content
+        with open(image_captcha_file, 'wb') as handler:
+            handler.write(img_captcha_data)
 
-    captcha_text = solve_captchas_with_model.solve_captcha_from_file(image_captcha_file)
-    response_check = session.post(url_check, data={'txtUserName': login, 'txtPassword': password, 'txtCaptcha': captcha_text, 'B1':'%E6%D1%E6%CF'}, timeout=(5, 30))
+        captcha_text = solve_captchas_with_model.solve_captcha_from_file(image_captcha_file)
+        response_check = session.post(url_check, data={'txtUserName': login, 'txtPassword': password, 'txtCaptcha': captcha_text, 'B1':'%E6%D1%E6%CF'}, timeout=(5, 30))
 
-    json_answer_empty = [{}]
-    if response_check.status_code == 200:
-        response_detailed_info = session.get(url_detailed_info)
-        response_detailed_info.encoding = 'windows-1256'
-        if response_detailed_info.status_code = 200:
-            full_answer = bs4(response_detailed_info.text, 'lxml')
-            fathers  = full_answer.find_all(id='AutoNumber2')
-            if (fathers):
-                father = fathers[0]
-                json_answer_list = []
-                childs_counter = 0
-                for first_child in father.children:
-                    if childs_counter > 3:
-                        if str(type(first_child)) == str_class_bs4_tag:
-                            json_answer_dict = {}
-                            field_order_position = 0
-                            for second_child in first_child.children:
-                                if str(type(second_child)) == str_class_bs4_tag:
-                                    one_operation = bs4(str(second_child), 'lxml')
-                                    wrapper = one_operation.find('font')
-                                    value = wrapper.text.strip()
-                                    json_answer_dict[html_fields[field_order_position]] = value
-                                    field_order_position += 1
-                            json_answer_list.append(json_answer_dict)
-                    childs_counter += 1
-                return str(json_answer_list)
+        if response_check.status_code == 200:
+            response_detailed_info = session.get(url_detailed_info)
+            response_detailed_info.encoding = 'windows-1256'
+            if response_detailed_info.status_code == 200:
+                full_answer = bs4(response_detailed_info.text, 'lxml')
+                fathers  = full_answer.find_all(id='AutoNumber2')
+                if (fathers):
+                    father = fathers[0]
+                    json_answer_list = []
+                    childs_counter = 0
+                    for first_child in father.children:
+                        if childs_counter > 3:
+                            if str(type(first_child)) == str_class_bs4_tag:
+                                json_answer_dict = {}
+                                field_order_position = 0
+                                for second_child in first_child.children:
+                                    if str(type(second_child)) == str_class_bs4_tag:
+                                        one_operation = bs4(str(second_child), 'lxml')
+                                        wrapper = one_operation.find('font')
+                                        value = wrapper.text.strip()
+                                        json_answer_dict[html_fields[field_order_position]] = value
+                                        field_order_position += 1
+                                json_answer_list.append(json_answer_dict)
+                        childs_counter += 1
+                    return str(json_answer_list)
+                else:
+                    return str(json_answer_empty)
             else:
                 return str(json_answer_empty)
         else:
